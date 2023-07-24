861a9cae1c631b103435426d828ff532480ea2cf
dkrtp <dkrtp@183.101.126.130>
2023-07-24 21:01:12 +0900

    게임판 랜덤 컬러 구현

--- toyProject1/src/main/webapp/js/simon.js
+++ toyProject1/src/main/webapp/js/simon.js
@@ -6,7 +6,7 @@ let bottomLeft = document.querySelector('.bottomLeft');
 let bottomRight = document.querySelector('.bottomRight');
 let squenceList = []; // 순서 저장 배열
 let colorList = [];	// 색상 횟수 저장 배열
-let level = 1; // 게임 레벨 변수
+let level = 5; // 게임 레벨 변수
 let gameRun = true;	// 게임 상태 변수
 
 topLeft.addEventListener("mousedown", topLeftOn);
@@ -43,6 +43,13 @@ function bottomRightOn(){
 	bottomLeft.style.backgroundColor="#FFD400";
 }
 
+function resetColor(){
+	bottomRight.style.backgroundColor=" #222889";
+	topLeft.style.backgroundColor="darkgreen";
+	topRight.style.backgroundColor="#BD2F2C";
+	bottomLeft.style.backgroundColor="#FFD400";
+}
+
 function startGame(){	// f start
 	if(gameRun == true){	// if start
 		console.log('게임시작');
@@ -50,23 +57,29 @@ function startGame(){	// f start
 		let delay = 0;
 
 		for(let i=0; i < level; i++){ // for start
-			let color = (Math.random())*3;
+			let color = Math.floor(Math.random() * 4);
+			console.log(color);
 			
 		    delay += 1000;
+		    if(i == 0){
+				delay = 0;
+			}
 			
 			if(color == 0){
-			    setTimeout(async () => {
-			
-			    topLeftOn();
-			
-			    }, delay);
+				console.log('초록색');
+			    setTimeout(function(){topLeftOn()}, delay);
 		    }
 		    else if(color == 1){
-				 setTimeout(async () => {
-			
-			     topRightOn();
-			
-			    }, delay);
+				console.log('빨간색');
+				setTimeout(function(){topRightOn()}, delay);
+			}
+			else if(color == 2){
+				console.log('노란색');
+				setTimeout(function(){bottomLeftOn()}, delay);
+			}
+			else if(color == 3){
+				console.log('파란색');
+				setTimeout(function(){bottomRightOn()}, delay);
 			}
 		
 		}	// for end
