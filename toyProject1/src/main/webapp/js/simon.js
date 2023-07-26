cae841efbe81c9d5d53ba4f33f0b0897637525b2
504 <504@DESKTOP-PI60H60>
2023-07-26 18:00:32 +0900

    07-26 사이먼게임 게임동작완성

--- toyProject1/src/main/webapp/js/simon.js
+++ toyProject1/src/main/webapp/js/simon.js
@@ -6,21 +6,23 @@ let bottomLeft = document.querySelector('.bottomLeft');
 let bottomRight = document.querySelector('.bottomRight');
 let squenceList = []; // 순서 저장 배열
 let colorList = [];	// 색상 횟수 저장 배열
-let level = 5; // 게임 레벨 변수
+let level = 1; // 게임 레벨 변수
+let squenceCount = 0;
 let gameRun = true;	// 게임 상태 변수
 
-topLeft.addEventListener("mousedown", topLeftOn);
-topRight.addEventListener("mousedown", topRightOn);
-bottomLeft.addEventListener("mousedown", bottomLeftOn);
-bottomRight.addEventListener("mousedown", bottomRightOn);
 
-/*클릭시 색상 변경*/
+topLeft.addEventListener("mousedown", topLeftOnClick);
+topRight.addEventListener("mousedown", topRightOnClick);
+bottomLeft.addEventListener("mousedown", bottomLeftOnClick);
+bottomRight.addEventListener("mousedown", bottomRightOnClick);
+
+/*게임판 색상변경 함수*/
 function topLeftOn(){
 	topLeft.style.backgroundColor=" #44f028";
 	topRight.style.backgroundColor="#BD2F2C";
 	bottomLeft.style.backgroundColor="#FFD400";
 	bottomRight.style.backgroundColor="#222889";
-	setTimeout(function(){resetColor()}, 1000);	
+	setTimeout(function(){resetColor()}, 300);	
 }
 
 function topRightOn(){
@@ -28,7 +30,7 @@ function topRightOn(){
 	topLeft.style.backgroundColor="darkgreen";
 	bottomLeft.style.backgroundColor="#FFD400";
 	bottomRight.style.backgroundColor="#222889";
-	setTimeout(function(){resetColor()}, 1000);	
+	setTimeout(function(){resetColor()}, 300);	
 }
 
 function bottomLeftOn(){
@@ -36,7 +38,7 @@ function bottomLeftOn(){
 	topLeft.style.backgroundColor="darkgreen";
 	topRight.style.backgroundColor="#BD2F2C";
 	bottomRight.style.backgroundColor="#222889";
-	setTimeout(function(){resetColor()}, 1000);	
+	setTimeout(function(){resetColor()}, 300);	
 }
 
 function bottomRightOn(){
@@ -44,7 +46,104 @@ function bottomRightOn(){
 	topLeft.style.backgroundColor="darkgreen";
 	topRight.style.backgroundColor="#BD2F2C";
 	bottomLeft.style.backgroundColor="#FFD400";
-	setTimeout(function(){resetColor()}, 1000);	
+	setTimeout(function(){resetColor()}, 300);	
+}
+
+/*클릭시 색상 변경*/
+function topLeftOnClick(){
+	if(gameRun == false){
+		topLeft.style.backgroundColor=" #44f028";
+		topRight.style.backgroundColor="#BD2F2C";
+		bottomLeft.style.backgroundColor="#FFD400";
+		bottomRight.style.backgroundColor="#222889";
+		setTimeout(function(){resetColor()}, 300);
+		if(squenceList[squenceCount] == 0){
+			squenceCount++;
+			if(squenceCount == squenceList.length){
+				level++;
+				squenceCount = 0;
+				squenceList = [];
+				gameRun = true;
+			}
+		}else{
+			squenceCount = 0;
+			squenceList = [];
+			level = 1;
+			gameRun = true;
+		}
+	}
+}
+
+function topRightOnClick(){
+	if(gameRun == false){
+		topRight.style.backgroundColor=" #FF2300";
+		topLeft.style.backgroundColor="darkgreen";
+		bottomLeft.style.backgroundColor="#FFD400";
+		bottomRight.style.backgroundColor="#222889";
+		setTimeout(function(){resetColor()}, 300);
+		if(squenceList[squenceCount] == 1){
+			squenceCount++;
+			if(squenceCount == squenceList.length){
+				level++;
+				squenceCount = 0;
+				squenceList = [];
+				gameRun = true;
+			}
+		}else{
+			squenceCount = 0;
+			squenceList = [];
+			level = 1;
+			gameRun = true;
+		}
+	}	
+}
+
+function bottomLeftOnClick(){
+	if(gameRun == false){
+		bottomLeft.style.backgroundColor=" #FFFF02";
+		topLeft.style.backgroundColor="darkgreen";
+		topRight.style.backgroundColor="#BD2F2C";
+		bottomRight.style.backgroundColor="#222889";
+		setTimeout(function(){resetColor()}, 300);
+		if(squenceList[squenceCount] == 2){
+			squenceCount++;
+			if(squenceCount == squenceList.length){
+				level++;
+				squenceCount = 0;
+				squenceList = [];
+				gameRun = true;
+			}
+		}else{
+			squenceCount = 0;
+			squenceList = [];
+			level = 1;
+			gameRun = true;
+		}
+	}	
+}
+
+function bottomRightOnClick(){
+	if(gameRun == false){
+		bottomRight.style.backgroundColor=" #0A03FC";
+		topLeft.style.backgroundColor="darkgreen";
+		topRight.style.backgroundColor="#BD2F2C";
+		bottomLeft.style.backgroundColor="#FFD400";
+		setTimeout(function(){resetColor()}, 300);
+		if(squenceList[squenceCount] == 3){
+			squenceCount++;
+			if(squenceCount == squenceList.length){
+				level++;
+				squenceCount = 0;
+				squenceList = [];
+				gameRun = true;
+			}
+		}else{
+			squenceCount = 0;
+			squenceList = [];
+			level = 1;
+			gameRun = true;
+		}	
+	}
 }
 
 // 모든 색판의 색을 원래대로 리셋시키는 함수
@@ -55,15 +154,17 @@ function resetColor(){
 	bottomLeft.style.backgroundColor="#FFD400";
 }
 
+
+
 function startGame(){	// f start
 	if(gameRun == true){	// if start
 		console.log('게임시작');
-		gameRun = false;
 		let delay = 0;
 
 		for(let i=0; i < level; i++){ // for start
 			let color = Math.floor(Math.random() * 4);
 			console.log(color);
+			delay += 1000;
 			
 			if(color == 0){
 				console.log('초록색');
@@ -85,10 +186,11 @@ function startGame(){	// f start
 				setTimeout(function(){bottomRightOn()}, delay);
 				squenceList.push(color);
 			}
-			delay += 1000;
 		
 		}	// for end
+		
 		console.log(squenceList);
+		setTimeout(function(){gameRun = false; console.log('상태바꿈')}, delay+1000);
 		
 	}	// if end
 }	// f end
